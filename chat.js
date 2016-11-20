b8511ce6bfa14a32ac6500d7039b917287731295
DESKTOP-PFEHFOU\admin <sholomka@gmail.com>
2016-11-20 23:26:13 +0200

    add

---
+++ chat.js
@@ -0,0 +1,21 @@
+let clients = [];
+
+exports.subscribe = (req, res) => {
+    console.log('subscribe');
+    clients.push(res);
+
+    res.on('close', () => {
+        clients.splice(clients.indexOf(res), 1);
+    });
+};
+
+exports.publish = (message) => {
+
+    console.log("publish '%s'", message);
+
+    clients.forEach((res) => {
+        res.end(message);
+    });
+
+    clients = [];
+};
